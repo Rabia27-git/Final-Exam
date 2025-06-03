3cfb5fbe5d0d94c332e71b22bc121ab947726ef6
Rabia27-bit <rabiatunio.bscsf22@iba-suk.edu.pk>
2025-06-03 11:39:54 +0500

    Light cycle logic

--- ead-finalexam/src/TrafficLightSimulator.jsx
+++ ead-finalexam/src/TrafficLightSimulator.jsx
@@ -22,6 +22,35 @@ function TrafficLightSimulator() {
         backgroundColor:"rgb(117, 106, 106)"
     }
 
+    const redColorStyles={
+        width:"50px",
+        height:"50px",
+        borderRadius:"50%",
+        marginTop:"15px",
+        marginLeft:"20px",
+        backgroundColor:"red"
+    }
+
+    const yellowColorStyles={
+        width:"50px",
+        height:"50px",
+        borderRadius:"50%",
+        marginTop:"15px",
+        marginLeft:"20px",
+        backgroundColor:"yellow"
+    }
+
+    const greenColorStyles={
+        width:"50px",
+        height:"50px",
+        borderRadius:"50%",
+        marginTop:"15px",
+        marginLeft:"20px",
+        backgroundColor:"green"
+    }
+
+
+    // Logic
     const [light,setLight]=useState("red")
     const [classColor,setClassColor]=useState("redColorStyles")
 
@@ -62,9 +91,9 @@ function TrafficLightSimulator() {
     <>
         <h1 style={{textAlign:"center"}}>Traffic Light Simulator</h1>
         <div style={boxStyles}>
-            <div style={circleStyles}></div>
-            <div style={circleStyles}></div>
-            <div style={circleStyles}></div>
+            {light==="red" ? <div style={redColorStyles}></div> : <div style={circleStyles}></div>}
+            {light==="yellow" ? <div style={yellowColorStyles}></div> : <div style={circleStyles}></div>}
+            {light==="green" ? <div style={greenColorStyles}></div> : <div style={circleStyles}></div>}
         </div>
     </>
   )
